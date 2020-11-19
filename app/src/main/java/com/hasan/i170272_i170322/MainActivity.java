150a64c3c883b293e7e3e1aaace83e0a032b4e4d
Raja-Salman-Tariq <i170322@nu.edu.pk>
2020-11-20 03:07:29 +0500

    Check for file along with MainActivity.java. This is a test to see if git is functioningg.

--- app/src/main/java/com/hasan/i170272_i170322/MainActivity.java
+++ app/src/main/java/com/hasan/i170272_i170322/MainActivity.java
@@ -1,10 +1,14 @@
 package com.hasan.i170272_i170322;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/hasan/i170272_i170322/RegisterActivity.java
@@ -0,0 +1,89 @@
+package com.hasan.i170272_i170322;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    AppCompatEditText uname, pwd;
+    AppCompatButton registerButton;
+
+    FirebaseAuth authentication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        authentication=FirebaseAuth.getInstance();
+
+        registerButton=findViewById(R.id.registerButton);
+        uname=findViewById(R.id.username);
+        pwd=findViewById(R.id.password);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                authentication.createUserWithEmailAndPassword(
+                        uname.getText().toString(),
+                        pwd.getText().toString()
+                )
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(
+                                    RegisterActivity.this,
+                                    "Registration Successfull !",
+                                    Toast.LENGTH_LONG
+                            ).show();
+                            Toast.makeText(
+                                    RegisterActivity.this,
+                                    authentication.getCurrentUser().getUid(),
+                                    Toast.LENGTH_LONG
+                            ).show();                        }
+                    }
+                })
+
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(
+                                RegisterActivity.this,
+                                "Failed to create user.",
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user=authentication.getCurrentUser();
+
+        if (user!=null){
+            Toast.makeText(
+                    RegisterActivity.this,
+                    user.getUid(),
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+
+    }
+}
